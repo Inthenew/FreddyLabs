94d0b1c5a0571dd3825b91735d03b6512156f8e7
JM <matthew@slocum.io>
2025-05-26 23:18:52 -0400

    Patch

--- nodle.js
+++ nodle.js
@@ -3,10 +3,23 @@
 // PRTIALLY AI WRITTEN - YEAH I KNOW, DISGUSTING, BUT IM LAZY //
 
 $(document).ready(function() {
+    // Cumputer //
     $('#title').hover(() => {
         // Slide the secreet message down from da title //
         $('#secret-message').stop(true, true).slideDown(300);
     }, () => {
         $('#secret-message').stop(true, true).slideUp(300);
+    });
+    
+    // Mobile //
+    $('#title').on('touchstart', function(e) {
+        e.preventDefault();
+        const secretMessage = $('#secret-message');
+        
+        if (secretMessage.is(':visible')) {
+            secretMessage.stop(true, true).slideUp(300);
+        } else {
+            secretMessage.stop(true, true).slideDown(300);
+        }
     });
 });
